f2bc7e31ede4f3d40f2cdcb7edc70cf40e51e4f3
rajesh <srajeshmohan@gmail.com>
2020-06-28 00:21:24 +0530

    Initial Commit

---
+++ components/Header/HeaderLinks.js
@@ -0,0 +1,85 @@
+/*eslint-disable*/
+import React from "react";
+import Link from "next/link";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+import Icon from "@material-ui/core/Icon";
+
+// @material-ui/icons
+import { Home, EmojiObjects } from "@material-ui/icons";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
+import Button from "components/CustomButtons/Button.js";
+
+import styles from "assets/jss/nextjs-material-kit/components/headerLinksStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderLinks(props) {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="#"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <Home className={classes.icons} /> Home
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="#"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <EmojiObjects className={classes.icons} /> Get Started
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-linkedIn"
+          title="Follow us on LinkedIn"
+          placement={"top"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            href="https://www.linkedin.com"
+            target="_blank"
+            color="transparent"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-linkedin"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-tooltip"
+          title="Follow us on Github"
+          placement={"top"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://github.com/maatram"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-github"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
---
+++ pages/components.js
@@ -0,0 +1,68 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react components for routing our app without refresh
+import Link from "next/link";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+// core components
+import Header from "components/Header/Header.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import Parallax from "components/Parallax/Parallax.js";
+import SectionLogin from "pages-sections/Components-Sections/SectionLogin.js";
+import styles from "assets/jss/nextjs-material-kit/pages/components.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Components(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        brand="Maatram"
+        rightLinks={<HeaderLinks />}
+        fixed
+        color="transparent"
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+        {...rest}
+      />
+      <Parallax image={require("assets/img/nextjs_header.jpg")}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem>
+              <div className={classes.brand}>
+                <h1 className={classes.title}>Together we can make a difference.</h1>
+                <h3 className={classes.subtitle}>
+                  An initiative opensource internship program for students
+                </h3>
+              </div>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <SectionLogin />
+        <GridItem md={12} className={classes.textCenter}>
+          <Link href="/login">
+            <a className={classes.link}>
+              <Button color="primary" size="lg" simple>
+                See more
+              </Button>
+            </a>
+          </Link>
+        </GridItem>
+      </div>
+      <Footer />
+    </div>
+  );
+}
